f73634d97359d0599955aa1d8bd143cd61d2adce
Josh <joshlowe.cs@gmail.com>
2022-10-17 16:42:01 +0000

    Updated hacked.py test 2

--- hackedNews.py
+++ hackedNews.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static_folder')
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+	return render_template('index.html')
+
+@app.rout('/test')
+def test():
+        return "hey"
 
 if __name__ == '__main__':
    app.run()
