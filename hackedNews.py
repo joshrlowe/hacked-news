dc227428feaa2c149604a61eaa5fb387a133b29b
trevorfagan77 <trevorfagan77.com>
2022-10-26 14:51:27 +0000

    Added calls to API and displaying the data

--- hackedNews.py
+++ hackedNews.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
+import requests
+
 app = Flask(__name__, static_folder='static_folder')
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+	payload = "{}"
+	response = requests.request("GET", url, data=payload)
 
-@app.rout('/test')
-def test():
-        return "hey"
+	temp = response.json()
+
+	responses = []
+
+	for article in temp[:30]:
+		url = "https://hacker-news.firebaseio.com/v0/item/{}.json".format(article)
+		payload = "{}"
+		response = requests.request("GET", url, data=payload)
+		responses.append(response.json())
+
+	return render_template('index.html', data=responses)
 
 if __name__ == '__main__':
    app.run()
