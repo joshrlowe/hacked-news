e3934f3482c95f606c8b314d3f11a70c4994677f
trevorfagan77 <trevorfagan77@gmail.com>
2022-11-03 23:12:52 +0000

    fixed the old bugs

--- hackedNews.py
+++ hackedNews.py
@@ -1,7 +1,32 @@
 from flask import Flask, render_template
 import requests
 
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
 app = Flask(__name__, static_folder='static_folder')
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
 
 @app.route('/')
 def home():
@@ -19,7 +44,42 @@ def home():
 		response = requests.request("GET", url, data=payload)
 		responses.append(response.json())
 
-	return render_template('index.html', data=responses)
+	return render_template('index.html', data=responses, session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route('/account')
+def account():
+        return render_template('account.html', session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+
+@app.route('/likes')
+def likes():
+        return render_template('likes.html', session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
 
 if __name__ == '__main__':
    app.run()
